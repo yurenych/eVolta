3b48d02e03dd285d708c8c701135cea1453184d4
Vlad Trishch <vlad.trishch@gmail.com>
2019-03-18 21:06:26 +0200

    Added an express server

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var fs = require('fs')
+var http = require('http')
+// var https = require('https')
+var app = express()
+
+// app.use(function(req, res, next) {
+//   if(!req.secure) {
+//     var secureUrl = "https://" + req.headers['host'] + req.url;
+//     res.writeHead(301, { "Location":  secureUrl });
+//     return res.end();
+//   }
+//   next();
+// })
+
+app.use(express.static('./'))
+
+// var credentials = {
+//   key: fs.readFileSync('security/private.key', 'utf8'),
+//   cert: fs.readFileSync('security/certificate.crt', 'utf8'),
+//   ca: fs.readFileSync('security/ca_bundle.crt', 'utf8'),
+// }
+
+var httpServer = http.createServer(app)
+// var httpsServer = https.createServer(credentials, app)
+
+httpServer.listen(80)
+// httpsServer.listen(443)
