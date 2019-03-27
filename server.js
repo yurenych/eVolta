291138c0056b7fd3e884cae16acbd23860c12e91
Vlad Trishch <vlad.trishch@gmail.com>
2019-03-27 21:11:56 +0200

    Added ssl support

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 var express = require('express')
 var fs = require('fs')
 var http = require('http')
-// var https = require('https')
+var https = require('https')
 var app = express()
 
-// app.use(function(req, res, next) {
-//   if(!req.secure) {
-//     var secureUrl = "https://" + req.headers['host'] + req.url;
-//     res.writeHead(301, { "Location":  secureUrl });
-//     return res.end();
-//   }
-//   next();
-// })
+app.use(function(req, res, next) {
+  if(!req.secure) {
+    var secureUrl = "https://" + req.headers['host'] + req.url;
+    res.writeHead(301, { "Location":  secureUrl });
+    return res.end();
+  }
+  next();
+})
 
-app.use(express.static('./'))
+app.use(express.static('build'))
 
-// var credentials = {
-//   key: fs.readFileSync('security/private.key', 'utf8'),
-//   cert: fs.readFileSync('security/certificate.crt', 'utf8'),
-//   ca: fs.readFileSync('security/ca_bundle.crt', 'utf8'),
-// }
+var credentials = {
+  key: fs.readFileSync('security/private.key', 'utf8'),
+  cert: fs.readFileSync('security/certificate.crt', 'utf8'),
+  ca: fs.readFileSync('security/ca_bundle.crt', 'utf8'),
+}
 
 var httpServer = http.createServer(app)
-// var httpsServer = https.createServer(credentials, app)
+var httpsServer = https.createServer(credentials, app)
 
 httpServer.listen(80)
-// httpsServer.listen(443)
+httpsServer.listen(443)
